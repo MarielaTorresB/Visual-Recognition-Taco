0f0c39821d9ecd34038b82b2df147b82d70fbf26
Mariela Torres <martz_maru@hotmail.com>
2019-08-01 20:41:00 -0500

    Modificando Manifest

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 //set IBM cloud credentials in this section
 var visualRecognition = new VisualRecognitionV3({
   version: '2019-05-29',
-  iam_apikey: 'Remplace aqui'
+  iam_apikey: 'g_3RtScZR-PyJjHyCUwjd5v_eNnJl8Xo89L64DqzOVHN'
 });
 
 // Set The Storage Engine
@@ -72,7 +72,7 @@ app.post('/upload', (req, res) => {
         });
       } else {
         var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
-        var classifier_ids = ["Tiposdetaco_2107577272"];
+        var classifier_ids = ["Tacos_385843504"];
         console.log("datos de la imagen -- " + images_file);
         //
           var params = {
